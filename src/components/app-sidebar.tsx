4af307af2ea9f1bab83f0ab6f43fdf61649b4b1f
Joshua Semana <joshuadrummer14@gmail.com>
2024-10-22 23:26:59 +0400

    feat: grouped levels populated in sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -6,33 +6,44 @@ import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './ui/collap
 import { ChevronDown } from 'lucide-react'
 import { Level, LevelGroup } from '@/payload-types'
 
-type GroupedLevels = {
-  [groupName: string]: Level[];
+type Group = {
+  name: string;
+  viewOrder: number;
+  levels: Level[];
 }
 
 const AppSidebar = () => {
-  const [levels, setLevels] = useState<GroupedLevels>({});
+  const [groupedLevels, setGroupedLevels] = useState<Group[]>([]);
 
   useEffect(() => {
     const fetchGradeLevels = async () => {
       try {
-        const data = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/levels`).then((res) => res.json());
+        const data = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/levels?limit=100`).then((res) => res.json());
         const levels: Level[] = data.docs;
+        const sortedLevels = levels.sort((a, b) => a.viewOrder - b.viewOrder);
 
-        const grouped = levels.reduce<GroupedLevels>((acc, level) => {
-          const group = level.group as LevelGroup;
-          const groupName = group.name;
+        let groupedLevels: Group[] = [];
 
-          if (!acc[groupName]) {
-            acc[groupName] = [];
+        sortedLevels.forEach(level => {
+          const levelGroup = level.group as LevelGroup;
+          const groupName = levelGroup.name;
+
+          if(!groupedLevels.some((group) => group.name === groupName)) {
+            groupedLevels.push({
+              name: groupName,
+              viewOrder: levelGroup.viewOrder,
+              levels: []
+            })
           }
-  
-          // Push the level into the appropriate group
-          acc[groupName].push(level);
-          return acc;
-        }, {});
 
-        setLevels(grouped);
+          const currentGroupIndex = groupedLevels.findIndex(group => group.name === groupName);
+
+          groupedLevels[currentGroupIndex].levels.push(level);
+        })
+
+        const sortedGroupedLevels = groupedLevels.sort((a, b) => a.viewOrder - b.viewOrder);
+
+        setGroupedLevels(sortedGroupedLevels);
       } catch (error) {
         console.error('AppSidebar:useEffect;' + error);
       }
@@ -42,34 +53,39 @@ const AppSidebar = () => {
   }, []);
 
   useEffect(() => {
-    console.log(levels)
-  }, [levels])
+    console.log(groupedLevels);
+  }, [groupedLevels])
 
   return (
     <Sidebar>
       <SidebarGroup>
         <SidebarGroupLabel>Grade Levels</SidebarGroupLabel>
         <SidebarGroupContent>
-          
-          {/* <SidebarMenu>
-            <Collapsible className="group/collapsible">
-              <SidebarMenuItem>
-                <CollapsibleTrigger asChild>
-                  <SidebarMenuButton>
-                    Elementary
-                    <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
-                  </SidebarMenuButton>
-                </CollapsibleTrigger>
-                <CollapsibleContent>
-                  <SidebarMenuSub>
-                    <SidebarMenuSubItem>
-                      <SidebarMenuButton>Grade 1</SidebarMenuButton>
-                    </SidebarMenuSubItem>
-                  </SidebarMenuSub>
-                </CollapsibleContent>
-              </SidebarMenuItem>
-            </Collapsible>
-          </SidebarMenu> */}
+          {groupedLevels.map((group, index) => (
+            <SidebarMenu key={index}>
+              <Collapsible className='group/collapsible'>
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild>
+                    <SidebarMenuButton>
+                      {group.name}
+                      <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                    </SidebarMenuButton>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    {group.levels.map((level, index) => (
+                      <SidebarMenuSub key={index}>
+                        <SidebarMenuSubItem>
+                          <SidebarMenuButton>
+                            {level.name}
+                          </SidebarMenuButton>
+                        </SidebarMenuSubItem>
+                      </SidebarMenuSub>
+                    ))}
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            </SidebarMenu>
+          ))}
         </SidebarGroupContent>
       </SidebarGroup>
     </Sidebar>
